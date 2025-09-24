01bcacbbe2ddc8b890672d1cb4170bbdb6189aba
elfroot-sj <eleonora.arcuri@stevejobs.academy>
2025-09-25 01:08:58 +0200

    chore(ex1, ex2)

--- Math_Upgraded/src/main/java/com/example/math_upgraded/Controller.java
+++ Math_Upgraded/src/main/java/com/example/math_upgraded/Controller.java
@@ -15,26 +15,27 @@ public class Controller {
     private final MathService mathService;
     public Controller(MathService mathService) {
         this.mathService = mathService;
-    }
-    //localhost:8080/math/add/2/3
+    } //costruttore
+
+    // localhost:8080/math/add/2/3
     @GetMapping("/add/{a}/{b}")
     public int add(@PathVariable int a, @PathVariable int b) {
         return mathService.add(a, b);
     }
 
-    //localhost:8080/math/subtract/10/3
+    // localhost:8080/math/subtract/10/3
     @GetMapping("/subtract/{a}/{b}")
     public int subtract(@PathVariable int a, @PathVariable int b) {
         return mathService.subtract(a, b);
     }
 
-    //localhost:8080/math/multiply/2/3
+    // localhost:8080/math/multiply/2/3
     @GetMapping("/multiply/{a}/{b}")
     public int multiply(@PathVariable int a, @PathVariable int b) {
         return mathService.multiply(a, b);
     }
 
-    //localhost:8080/math/divide/10/2
+    // localhost:8080/math/divide/10/2
     @GetMapping("/divide/{a}/{b}")
     public String divide(@PathVariable float a, @PathVariable float b) {
         return mathService.divide(a, b);
--- Math_Upgraded_es2/src/main/java/com/example/math_upgraded_es2/Controller.java
+++ Math_Upgraded_es2/src/main/java/com/example/math_upgraded_es2/Controller.java
@@ -5,33 +5,32 @@ import org.springframework.web.bind.annotation.*;
 @org.springframework.stereotype.Controller
 
 @RestController //dichiara questa classe come controller per gestire le richieste HTTP
-//@RequestMapping("/math") //imposta una rotta "di base" da cui partire
 public class Controller {
 
     private final MathService mathService;
     public Controller(MathService mathService) {
         this.mathService = mathService;
     }
-    //localhost:8080/math/add/2/3
+
+    // localhost:8080/add/2/3
     @RequestMapping("/add/{a}/{b}")
     public int add(@PathVariable int a, @PathVariable int b) {
         return mathService.add(a, b);
     }
 
-    //localhost:8080/subtract?a=10&b=3
+    // localhost:8080/subtract?a=10&b=3
     @GetMapping("/subtract")
     public int subtract(@RequestParam int a, @RequestParam int b) {
         return mathService.subtract(a, b);
     }
 
-    //localhost:8080/multiply
+    // localhost:8080/multiply
     @PostMapping("/multiply")
     public int multiply(@RequestBody JsonOperands operands) {
         return mathService.multiply(operands.getA(), operands.getB());
     }
 
-
-    //localhost:8080/math/divide/10/2
+    // localhost:8080/divide/10/2
     @RequestMapping("/divide/{a}/{b}")
     public String divide(@PathVariable float a, @PathVariable float b) {
         return mathService.divide(a, b);
